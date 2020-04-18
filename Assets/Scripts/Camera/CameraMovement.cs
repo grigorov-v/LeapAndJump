7e8c554d414e49ec087f261dcb1c44475a9e9dd7
valerii.grigorov <valerii.grigorov@matryoshkagames.com>
2020-04-18 18:38:45 +0700

    Параллакс. Постоянная ширина камеры. Подсчет нижней границы

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public float     Lerp      = 10;
+    public Vector2   offset    = Vector2.zero;
+    public Transform EdgePoint = null;
+    
+    Transform _player = null;
+    Camera    _camera = null;
+
+    void Awake() {
+        _player = FindObjectOfType<PlayerMovement>()?.transform;
+        _camera = GetComponent<Camera>();
+    }
+
+    void FixedUpdate() {
+        var curPos = transform.position;
+        curPos = Vector2.Lerp(curPos, _player.position, Lerp * Time.deltaTime);
+        curPos += (Vector3)offset;
+        curPos.z = -10;
+        curPos.x = 0;
+        curPos.y = Mathf.Clamp(curPos.y, CalculateMinYPos(), curPos.y);
+        transform.position = curPos;
+    }
+
+    float CalculateMinYPos() {
+        var bottomLeft = _camera.ScreenToWorldPoint(new Vector2 (0, 0));
+        var minYPos = EdgePoint.position.y + (transform.position.y - bottomLeft.y);
+        return minYPos;
+    }
+}
---
+++ Assets/Scripts/Camera/CameraScaler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraScaler : MonoBehaviour {
+    public Vector2 DefaultResolution = new Vector2(720, 1280);
+    
+    [Range(0f, 1f)] 
+    public float   WidthOrHeight     = 0;
+
+    Camera _camera        = null;
+    float  _initialSize   = 0;
+    float  _targetAspect  = 0;
+    float  _initialFov    = 0;
+    float  _horizontalFov = 120f;
+
+    void Start() {
+        _camera = GetComponent<Camera>();
+        _initialSize = _camera.orthographicSize;
+        _targetAspect = DefaultResolution.x / DefaultResolution.y;
+        _initialFov = _camera.fieldOfView;
+        _horizontalFov = CalcVerticalFov(_initialFov, 1 / _targetAspect);
+    }
+
+    void Update() {
+        if (_camera.orthographic) {
+            var constantWidthSize = _initialSize * (_targetAspect / _camera.aspect);
+            _camera.orthographicSize = Mathf.Lerp(constantWidthSize, _initialSize, WidthOrHeight);
+        } else {
+            var constantWidthFov = CalcVerticalFov(_horizontalFov, _camera.aspect);
+            _camera.fieldOfView = Mathf.Lerp(constantWidthFov, _initialFov, WidthOrHeight);
+        }
+    }
+
+    float CalcVerticalFov(float hFovInDeg, float aspectRatio) {
+        var hFovInRads = hFovInDeg * Mathf.Deg2Rad;
+        var vFovInRads = 2 * Mathf.Atan(Mathf.Tan(hFovInRads / 2) / aspectRatio);
+        return vFovInRads * Mathf.Rad2Deg;
+    }
+}
