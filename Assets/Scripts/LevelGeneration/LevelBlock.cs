08380a7f100d08c1c951fb3d3812c576a37f6c25
valerii.grigorov <grigorov2205valera@gmail.com>
2020-04-26 16:22:35 +0700

    Grind для генерации уровней.

--- Assets/Scripts/LevelGeneration/LevelBlock.cs
+++ Assets/Scripts/LevelGeneration/LevelBlock.cs
@@ -9,6 +9,28 @@ public class LevelBlock : MonoBehaviour {
     public Transform BeginPoint = null;
     public Transform EndPoint   = null;
 
+    LevelGrind         _levelGrind    = null;
+    List<LevelElement> _levelElements = new List<LevelElement>();
+
+    LevelGrind LevelGrind {
+        get {
+            if ( !_levelGrind ) {
+                _levelGrind = GetComponentInChildren<LevelGrind>();
+            }
+            return _levelGrind;
+        }
+    }
+
+    List<LevelElement> LevelElements {
+        get {
+            if ( _levelElements.Count == 0 ) {
+                GetComponentsInChildren<LevelElement>(false, _levelElements);
+            }
+
+            return _levelElements;
+        }
+    }
+
     void OnValidate() {
         var boxTrigger = GetComponent<BoxCollider2D>();
         if ( !boxTrigger.isTrigger ) {
@@ -20,6 +42,23 @@ public class LevelBlock : MonoBehaviour {
         var player = other.GetComponent<PlayerControl>();
         if ( player ) {
             EventManager.Fire(new PlayerIntoBlockTriggerEnter(this, player));
+        }
+    }
+
+    void OnDrawGizmos() {
+        if ( !LevelGrind ) {
+            return;
+        }
+        
+        var levelElements = LevelElements;
+        var boundsArray = LevelGrind.BoundsArray;
+        for ( var y = 0; y < LevelGrind.CellCount.y; y++ ) {
+            for ( var x = 0; x < LevelGrind.CellCount.x; x++ ) {
+                var bounds = boundsArray[x, y];
+                var color = levelElements.Exists(elem => elem.CommonBounds.Intersects(bounds)) ? Color.red : Color.green;
+                Gizmos.color = color;
+                Gizmos.DrawWireCube(bounds.center, bounds.size);
+            }
         }
     }
 }
---
+++ Assets/Scripts/LevelGeneration/LevelGrind.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LevelGrind : MonoBehaviour {
+    public Vector2    CellSize    = Vector2.zero;
+    public Vector2    StartCenter = Vector2.zero;
+    public Vector2Int CellCount   = Vector2Int.zero;
+
+    Vector2    _lastCellSize    = Vector2.zero;
+    Vector2    _lastStartCenter = Vector2.zero;
+    Vector2Int _lastCellCount   = Vector2Int.zero;
+
+    Bounds[,] _boundsArray = null;
+
+    public Bounds[,] BoundsArray {
+        get {
+            if ( (_boundsArray == null) || CheckChanged() ) {
+                GrindGenerate();
+            }
+            return _boundsArray;
+        }
+    }
+
+    void GrindGenerate() {
+        _boundsArray = new Bounds[CellCount.x, CellCount.y];
+        for ( var y = 0; y < CellCount.y; y++ ) {
+            for ( var x = 0; x < CellCount.x; x++ ) {
+                var center = transform.TransformPoint(StartCenter);
+                center.x += CellSize.x * x;
+                center.y += CellSize.y * y;
+                _boundsArray[x,y] = new Bounds(center, CellSize);
+            }
+        }
+
+        _lastCellSize = CellSize;
+        _lastStartCenter = StartCenter;
+        _lastCellCount = CellCount;
+    }
+
+    bool CheckChanged() {
+        return (_lastCellSize != CellSize) || (_lastStartCenter != StartCenter) || (_lastCellCount != CellCount);
+    }
+}
