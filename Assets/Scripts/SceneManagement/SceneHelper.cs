fc1e574c020415e9cdb5d0311eb4b7314537284e
valerii.grigorov <grigorov2205valera@gmail.com>
2020-05-09 18:46:17 +0700

    Update SceneHelper.cs

--- Assets/Scripts/SceneManagement/SceneHelper.cs
+++ Assets/Scripts/SceneManagement/SceneHelper.cs
@@ -1,11 +1,23 @@
-﻿namespace Grigorov.SceneManagement {
+﻿using System.Collections.Generic;
+
+namespace Grigorov.SceneManagement {
     public static class SceneHelper {
+        static Dictionary<string, SceneHandler> _sceneHandlers = new Dictionary<string, SceneHandler>();
+
         public static SceneHandler LoadSceneAsync(string sceneName) {
-            return new SceneHandler(sceneName).LoadSceneAsync();
+            return GetSceneHandler(sceneName).LoadSceneAsync();
         }
 
         public static SceneHandler LoadScene(string sceneName) {
-            return new SceneHandler(sceneName).LoadScene();
+            return GetSceneHandler(sceneName).LoadScene();
+        }
+
+        static SceneHandler GetSceneHandler(string sceneName) {
+            if ( !_sceneHandlers.ContainsKey(sceneName) ) {
+                _sceneHandlers[sceneName] = new SceneHandler(sceneName);
+            }
+
+            return _sceneHandlers[sceneName];
         }
     }
 }
