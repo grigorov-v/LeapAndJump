20fab2fdd18b6539f074abf51748eb07a23f4e33
valerii.grigorov <grigorov2205valera@gmail.com>
2020-05-10 19:22:29 +0700

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Unity\Hub\Editor\2018.4.21f1\Editor\Unity.exe" -quit -batchmode -projectPath "E:\Projects\LeapAndJump" -executeMethod CustomBuildPipeline.BuildPlayer.ClearApkBuildDirectory
-"C:\Program Files\Unity\Hub\Editor\2018.4.21f1\Editor\Unity.exe" -quit -batchmode -projectPath "E:\Projects\LeapAndJump" -executeMethod CustomBuildPipeline.BuildPlayer.AndroidBuild
+"C:\Program Files\Unity\Hub\Editor\2018.4.21f1\Editor\Unity.exe" -quit -batchmode -projectPath "E:\Projects\LeapAndJump" -executeMethod Grigorov.CustomBuildPipeline.BuildPlayer.ClearApkBuildDirectory
+"C:\Program Files\Unity\Hub\Editor\2018.4.21f1\Editor\Unity.exe" -quit -batchmode -projectPath "E:\Projects\LeapAndJump" -executeMethod Grigorov.CustomBuildPipeline.BuildPlayer.AndroidBuild
