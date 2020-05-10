1cfe351a0f277937ff6346f5cab2e7e3c29330e3
valerii.grigorov <grigorov2205valera@gmail.com>
2020-05-10 11:21:27 +0700

    TestLoadingBar

---
+++ Assets/Scripts/ForTest/TestLoadingBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using Grigorov.SceneManagement;
+
+public class TestLoadingBar : MonoBehaviour
+{
+    [SerializeField] Image _fill = null;
+
+    void Start()
+    {
+        SceneHelper.LoadSceneAsync("MainMenu")
+            .SetLoadingAction((progress) => _fill.fillAmount = progress);
+    }
+}
