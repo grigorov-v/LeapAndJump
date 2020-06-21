e2af08d34ea4bced7e9a097b872865c19b5ebfdb
valerii.grigorov <valerii.grigorov@matryoshka.com>
2020-06-21 23:48:27 +0700

    Оболочка для сохранения полей

---
+++ Assets/Scripts/Save/SaveableField.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Grigorov.Save {
+    public class SaveableField<T> {
+        [System.Serializable]
+        struct ValueContainer {
+            public T Value;
+        }
+
+        string         _key            = string.Empty;
+        ValueContainer _valueContainer = new ValueContainer();
+
+        public T Value {
+            get => _valueContainer.Value;
+            set => _valueContainer.Value = value;
+        }
+
+        public SaveableField(string key) {
+            _key = key;
+        }
+
+        public void Load(T defaultValue = default) {
+            var json = PlayerPrefs.GetString(_key, null);
+            if ( string.IsNullOrEmpty(json) ) {
+                _valueContainer = new ValueContainer();
+                _valueContainer.Value = defaultValue;
+                return;
+            }
+            
+            _valueContainer = JsonUtility.FromJson<ValueContainer>(json);
+        }
+
+        public void Save() {
+            var json = JsonUtility.ToJson(_valueContainer);
+            PlayerPrefs.SetString(_key, json);
+        }
+    }
+}
