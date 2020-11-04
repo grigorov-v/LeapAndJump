6021ee03074734d33d148c0c2cc5d7eb0283e462
Grigorov <grigorov2205valera@gmail.com>
2020-11-04 22:26:39 +0700

    Add events submodule

--- Assets/Scripts/Common/Events/EventCallbackException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Grigorov.Unity.Events {
-	public sealed class EventCallbackException : Exception {
-		public EventCallbackException(Type eventType, Exception innerException) :
-			base(string.Format("Failed to call event callback for {0}", eventType.Name), innerException) {}
-	}
-}
